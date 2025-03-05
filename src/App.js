d6deffe3678fa6a9ba80c27c5ec52b22d85aee5a
Julia Semenova <julie.loggi@gmail.com>
2025-03-05 16:25:03 +0000

    setup prettier (+ add gitignore)

--- src/App.js
+++ src/App.js
@@ -5,31 +5,27 @@ const Pizza = (props) => {
     React.createElement("div", {}, [
       React.createElement("h2", null, props.name),
       React.createElement("p", null, props.description),
-    ])
+    ]),
   );
-}
+};
 
 const App = () => {
-  return React.createElement(
-    "div",
-    {},
-    [
-      React.createElement("h1", {}, "Padre Gino's"),
-      React.createElement(Pizza, {
-        name: "The Pepperoni Pizza",
-        description: "Mozzarella Cheese, Pepperoni",
-      }),
-      React.createElement(Pizza, {
-        name: "The Hawaiian Pizza",
-        description: "Sliced Ham, Pineapple, Mozzarella Cheese",
-      }),
-      React.createElement(Pizza, {
-        name: "The Big Meat Pizza",
-        description: "Bacon, Pepperoni, Italian Sausage, Chorizo Sausage",
-      }),
-    ]
-  )
-}
+  return React.createElement("div", {}, [
+    React.createElement("h1", {}, "Padre Gino's"),
+    React.createElement(Pizza, {
+      name: "The Pepperoni Pizza",
+      description: "Mozzarella Cheese, Pepperoni",
+    }),
+    React.createElement(Pizza, {
+      name: "The Hawaiian Pizza",
+      description: "Sliced Ham, Pineapple, Mozzarella Cheese",
+    }),
+    React.createElement(Pizza, {
+      name: "The Big Meat Pizza",
+      description: "Bacon, Pepperoni, Italian Sausage, Chorizo Sausage",
+    }),
+  ]);
+};
 
 const container = document.getElementById("root");
 const root = ReactDOM.createRoot(container);
