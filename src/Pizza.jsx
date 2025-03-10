f30315392649dac2b14e9dd8667d0655d1b58bd6
Julia Semenova <julie.loggi@gmail.com>
2025-03-10 16:37:23 +0000

    adding static assets

--- src/Pizza.jsx
+++ src/Pizza.jsx
@@ -3,6 +3,7 @@ const Pizza = (props) => {
     <div className="pizza">
       <h2>{props.name}</h2>
       <p>{props.description}</p>
+      <img src={props.image} alt={props.name} />
     </div>
   );
 };
