d557809333c815e58940b8921e110e2f5453885e
Julia Semenova <julie.loggi@gmail.com>
2025-03-10 16:07:35 +0000

    configuring eslint for jsx

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,9 +1,19 @@
 import js from "@eslint/js";
 import globals from "globals";
 import prettier from "eslint-config-prettier";
+import reactPlugin from "eslint-plugin-react";
 
 export default [
   js.configs.recommended,
+  {
+    ...reactPlugin.configs.flat.recommended,
+    settings: {
+      react: {
+        version: "detect",
+      },
+    },
+  },
+  reactPlugin.configs.flat["jsx-runtime"],
   {
     files: ["**/*.js", "**/*.jsx"],
     languageOptions: {
@@ -13,6 +23,10 @@ export default [
           jsx: true,
         },
       },
+    },
+    rules: {
+      "react/no-unescaped-entities": "off",
+      "react/prop-types": "off",
     },
   },
   prettier,
